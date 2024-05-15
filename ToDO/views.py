13588c639f436979bd75f66323b7c2048fe1134d
Bibash <workinprogress.bibash@gmail.com>
2024-05-15 21:38:54 +0545

    Frontend to backend data transfer

--- ToDO/views.py
+++ ToDO/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
+    if request.method == "POST":
+        data = request.POST
+        Task = data.get("Task")
+        TODO.objects.create(
+            Task=Task,
+        )
     return render(request, "home.html")
