667fe50c1bd5c568dcf5c74b685355ce99e97475
Bibash <workinprogress.bibash@gmail.com>
2024-05-18 21:24:50 +0545

    Add feature: >> Added update operation >> Improved Frontend

--- ToDO/urls.py
+++ ToDO/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('delete_Task/<int:id>/', views.delete_Task, name="delete_Task",),
+    path('update_Task/<int:id>/', views.update_Task, name="update_Task",),
 
 
 ]
--- ToDO/views.py
+++ ToDO/views.py
@@ -12,9 +12,10 @@ def home(request):
         TODO.objects.create(
             Task=Task,
         )
-        return redirect('/')
+
     queryset = TODO.objects.all()
     context = {"Task": queryset}
+    return redirect('/')
     return render(request, "home.html", context)
 
 
@@ -22,3 +23,54 @@ def delete_Task(request, id):
     queryset = TODO.objects.get(id=id)
     queryset.delete()
     return redirect('/')
+
+
+def update_Task(request, id):
+    queryset = TODO.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        Task = data.get("Task")
+        queryset.Task = Task
+        queryset.save()
+        from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        data = request.POST
+        Task = data.get("Task")
+        TODO.objects.create(
+            Task=Task,
+        )
+        return redirect('/')
+    queryset = TODO.objects.all()
+    context = {"Task": queryset}
+    
+    return render(request, "home.html", context)
+
+
+def delete_Task(request, id):
+    queryset = TODO.objects.get(id=id)
+    queryset.delete()
+    return redirect('/')
+
+
+def update_Task(request, id):
+    queryset = TODO.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        Task = data.get("Task")
+        queryset.Task = Task
+        queryset.save()
+        return redirect('/')
+
+    context = {"Task": queryset}
+
+    return render(request, "update.html", context)
+
+    context = {"Task": queryset}
+
+    return render(request, "update.html", context)
