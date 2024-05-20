9e20e6a4860a01986bb3a2feb41f81aac71a5c6f
Bibash <workinprogress.bibash@gmail.com>
2024-05-20 13:52:54 +0545

    Fix registration form validation

--- ToDO/models.py
+++ ToDO/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class TODO(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
     Task = models.CharField(max_length=255)
-    
-    
+
     def __str__(self):
         return self.Task
--- ToDO/views.py
+++ ToDO/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -41,6 +43,29 @@ def update_Task(request, id):
 
 
 def register(request):
+    if request.method == "POST":
+        data = request.POST
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        username = data.get("username")
+        password = data.get("password")
+
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request, "Username already exists!")
+            return redirect("/register")
+
+        else:
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                password=password,
+            )
+        user.save()
+        messages.success(request, "Account registerd successfully!")
+        return redirect("login")
+
     return render(request, "register.html")
 
 
